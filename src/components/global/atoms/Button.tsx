c4fe1754e258df74e6d6e0995af951e446042527
sanjiv kumar <sanjivkumarhzb1427@gmail.com>
2024-04-09 22:42:36 +0530

    "commit frontend"

--- src/components/global/atoms/Button.tsx
+++ src/components/global/atoms/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { cva } from "class-variance-authority";
 import { cn } from "../../../lib/utils";
-
+/////////
 interface ButtonProps {
   children: React.ReactNode;
   onClick?: () => void;
